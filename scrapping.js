adc99fdd53c253ae3da0bd07d2455036045a25ed
Rodrigo Hashimoto <rodhash@gmail.com>
2022-09-30 09:19:54 -0300

    fix: Run all async scans (scrap) in order to avoid IP block
    Signed-off-by: Rodrigo Hashimoto <rodhash@gmail.com>

--- scrapping.js
+++ scrapping.js
@@ -44,7 +44,7 @@ async function getSena() {
                 console.log('MegaSena:', result)
                 await browser.close()
             } else{
-                n++
+                n=n+1
             }
 
         } catch(err){
@@ -58,159 +58,277 @@ async function getSena() {
 async function getQuina() {
     const browser  = await pup.launch()
     const page     = await browser.newPage()
-    await page.goto(urlQuina)
-    const result   = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll('#ulDezenas')).map(x => x.textContent)[0].trim().match(/.{1,2}/g)
-    })
-    await browser.close()
+
+    let runScan = true
+    let result
+    let n = 1
+
+    while (runScan) {
+        try {
+            await page.goto(urlQuina)
+            result = await page.evaluate(() => {
+                return Array.from(document.querySelectorAll('#ulDezenas')).map(x => x.textContent)[0].trim().match(/.{1,2}/g)
+            })
+            if (result) {
+                runScan = false
+                console.log('Quina: ', result)
+                await browser.close()
+            }
+            else {
+                n = n+1
+            }
+
+        } catch(e) {
+        }
+    }
     return result
 }
 
 async function getLotoFacil() {
     const browser = await pup.launch()
     const page = await browser.newPage()
-    await page.goto(urlLotoFacil)
-    const result = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll('#wp_resultados > div.content-section.section-text.with-box.column-left.no-margin-top > div > div > div:nth-child(3)')).map(x => x.textContent)[0].trim().match(/.{1,2}/g)
-    })
-    await browser.close()
+
+    let runScan = true
+    let result
+    let n = 1
+
+    while (runScan) {
+        try {
+            await page.goto(urlLotoFacil)
+            result = await page.evaluate(() => {
+                return Array.from(document.querySelectorAll('#wp_resultados > div.content-section.section-text.with-box.column-left.no-margin-top > div > div > div:nth-child(3)')).map(x => x.textContent)[0].trim().match(/.{1,2}/g)
+            })
+            if (result) {
+                runScan = false
+                console.log('LotoFacil: ', result)
+                await browser.close()
+            }
+            else {
+                n = n+1
+            }
+        } catch(e){}
+    }
     return result
+
 }
 
 async function getLotoMania() {
     const browser = await pup.launch()
     const page = await browser.newPage()
-    await page.goto(urlLotoMania)
-    const result = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll('#wp_resultados > div.content-section.section-text.with-box.column-left.no-margin-top > div > div > div:nth-child(3)')).map(x => x.textContent)[0].trim().match(/.{1,2}/g).map(x => x.trim()).filter(x => x !== '')
-    })
-    await browser.close()
+
+    let runScan = true
+    let result
+    let n = 1
+
+    while (runScan) {
+        try {
+            await page.goto(urlLotoMania)
+            result = await page.evaluate(() => {
+                return Array.from(document.querySelectorAll('#wp_resultados > div.content-section.section-text.with-box.column-left.no-margin-top > div > div > div:nth-child(3)')).map(x => x.textContent)[0].trim().match(/.{1,2}/g).map(x => x.trim()).filter(x => x !== '')
+            })
+            if (result) {
+                runScan = false
+                console.log('LotoMania: ', result)
+                await browser.close()
+            }
+            else {
+                n = n+1
+            }
+        } catch(e) {}
+    }
     return result
+
 }
 
 async function getSuperSete() {
     const browser = await pup.launch()
     const page = await browser.newPage()
-    await page.goto(urlSuperSete)
-    const result = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll('#ulDezenas')).map(x => x.textContent)[0].trim().match(/.{1,2}/g).map(x => x.trim()).filter(x => x !== '').join('').match(/.{1,2}/g).map(x => x[1])
-    })
-    await browser.close()
+
+    let runScan = true
+    let result
+    let n = 1
+
+    while (runScan) {
+        try {
+            await page.goto(urlSuperSete)
+            result = await page.evaluate(() => {
+                return Array.from(document.querySelectorAll('#ulDezenas')).map(x => x.textContent)[0].trim().match(/.{1,2}/g).map(x => x.trim()).filter(x => x !== '').join('').match(/.{1,2}/g).map(x => x[1])
+            })
+            // console.log(`result ${n}: `, result)
+            if (result) {
+                runScan = false
+                console.log('SuperSete: ', result)
+                await browser.close()
+            }
+            else {
+                n = n+1
+            }
+        } catch(e) {}
+    }
     return result
 }
 
-async function getDuplaSena() {
+async function getDuplaSena1() {
     const browser = await pup.launch()
     const page = await browser.newPage()
-    await page.goto(urlDuplaSena)
-    const result = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll('#wp_resultados > div.content-section.section-text.with-box.column-left.no-margin-top > div > div > ul:nth-child(3)')).map(x => x.textContent)[0].replace('1º sorteio', '').trim().match(/.{1,2}/g)
-    })
-    await browser.close()
+
+    let runScan = true
+    let result
+    let n = 1
+
+    while (runScan) {
+        try {
+            await page.goto(urlDuplaSena)
+            result = await page.evaluate(() => {
+                return Array.from(document.querySelectorAll('#wp_resultados > div.content-section.section-text.with-box.column-left.no-margin-top > div > div > ul:nth-child(3)')).map(x => x.textContent)[0].replace('1º sorteio', '').trim().match(/.{1,2}/g)
+            })
+
+            if (result) {
+                runScan = false
+                console.log('Dupla Sena1: ', result)
+                await browser.close()
+            }
+            else {
+                n = n+1
+            }
+        } catch(e) {}
+    }
     return result
 }
+
+async function getDuplaSena2() {
+    const browser = await pup.launch()
+    const page = await browser.newPage()
+
+    let runScan = true
+    let result
+    let n = 1
+
+    while (runScan) {
+        try {
+            await page.goto(urlDuplaSena)
+            result = await page.evaluate(() => {
+                return Array.from(document.querySelectorAll('#wp_resultados > div.content-section.section-text.with-box.column-left.no-margin-top > div > div > ul:nth-child(4)')).map(x => x.textContent)[0].replace('2º sorteio', '').trim().match(/.{1,2}/g)
+            })
+
+            if (result) {
+                runScan = false
+                console.log('Dupla Sena2: ', result)
+                await browser.close()
+            }
+            else {
+                n = n+1
+            }
+        } catch(e) {}
+    }
+    return result
+}
+
+
 
 async function getLoteca() {
     const browser = await pup.launch()
     const page = await browser.newPage()
-    await page.goto(urlLoteca)
-    const result = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll('#wp_resultados > div.content-section.section-text.with-box.column-left.no-margin-top > div > div > table > tbody')).map(x => x.textContent)
-    })
-    await browser.close()
-    // console.log(result)
 
+    let runScan = true
+    let result
+    let n = 1
+
+    while (runScan) {
+        try {
+            await page.goto(urlLoteca)
+            result = await page.evaluate(() => {
+                return Array.from(document.querySelectorAll('#wp_resultados > div.content-section.section-text.with-box.column-left.no-margin-top > div > div > table > tbody')).map(x => x.textContent)
+            })
+            if (result) {
+                runScan = false
+                // console.log('Loteca: ', result)
+                await browser.close()
+            }
+            else {
+                n = n+1
+            }
+        } catch(e) {}
+    }
     return result
 }
 
 async function getDiaDeSorte() {
     const browser = await pup.launch()
     const page = await browser.newPage()
-    await page.goto(urlDiaDeSorte)
-    const result = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll('#ulDezenas')).map(x => x.textContent)[0].trim().match(/.{1,2}/g)
-    })
-    await browser.close()
+
+    let runScan = true
+    let result
+    let n = 1
+
+    while (runScan) {
+        try {
+            await page.goto(urlDiaDeSorte)
+            result = await page.evaluate(() => {
+                return Array.from(document.querySelectorAll('#ulDezenas')).map(x => x.textContent)[0].trim().match(/.{1,2}/g)
+            })
+            if (result) {
+                runScan = false
+                console.log('Dia de Sorte: ', result)
+                await browser.close()
+            }
+            else {
+                n = n+1
+            }
+        } catch(e) {}
+    }
     return result
 }
 
 async function getTimeMania() {
     const browser = await pup.launch()
     const page = await browser.newPage()
-    await page.goto(urlTimeMania)
 
-    let scanStatus = true
-    let scanNumber = 1
-    while (scanStatus) {
+    let runScan = true
+    let result
+    let n = 1
+
+    while (runScan) {
         try {
-            const result = await page.evaluate(() => {
+            await page.goto(urlTimeMania)
+            result = await page.evaluate(() => {
                 return Array.from(document.querySelectorAll('#ulDezenas')).map(x => x.textContent)[0].trim().match(/.{1,2}/g)
             })
-            if (Array.isArray(result) && result.length > 0) {
-                scanStatus = false
-                console.log('Sena finished successfully')
-            } else {
-                throw new Error('Sena failed')
+
+            if (result) {
+                runScan = false
+                console.log('Time Mania: ', result)
+                await browser.close()
             }
-        } catch(e){
-            scanNumber++
-            console.log(`Error on Sena, trying again scan N#${scanNumber}..`)
-        }
+            else {
+                n = n+1
+            }
+        } catch(e){}
     }
-    await browser.close()
     return result
 }
 
+
+// --------------------------------------------------
+
+
+const today = new Date(Date.now()).toLocaleString("pt-BR", {weekday: "long", month: "long", day: "numeric", year: "numeric"})
+console.log(today)
+
+
 // getSena()
-    // .then(megaSena => {
-    //     console.log(typeof megaSena)
-    //     console.log(megaSena)
-    //
-    //     resSena.push(...megaSena)
-    //     allResults.push({MegaSena: resSena})
-    //
-    //     console.log(`Mega Sena: ${resSena}`)
-    //     console.log('All Results:', allResults)
-    //
-    //     fs.writeFile('/tmp/loterias.out', JSON.stringify({ allRes: allResults }), err => {
-    //         console.log(err)
-    //     })
-    // })
-
-getSena()
-
-/* getQuina()
-    .then(quina => {
-        resQuina.push(...quina)
-        console.log(`Quina: ${resQuina}`)
-    }) */
-
-/* getLotoFacil()
-    .then(res => {
-        resLotoFacil.push(...res)
-        console.log(`Loto Facil: ${resLotoFacil}`)
-    }) */
-
-/* getLotoMania()
-    .then(res => {
-        resLotoMania.push(...res)
-        console.log(resLotoMania)
-    }) */
-
-/* getSuperSete()
-    .then(res => {
-        resSuperSete.push(...res)
-        console.log(resSuperSete)
-    }) */
-
-/* getDuplaSena()
-    .then(res => {
-        resDuplaSena.push(...res)
-        console.log(resDuplaSena)
-    }) */
+// getQuina()
+// getLotoFacil()
+// getLotoMania()
+// getSuperSete()
+// getDuplaSena1()
+// getDuplaSena2()
+// getDiaDeSorte()
+// getTimeMania()
 
 // getLoteca()
 //     .then(result => {
+//         console.log('Loteca ...')
 //         const data = result[0].split('\n')
 //         const results = []
 //
@@ -234,17 +352,46 @@ getSena()
 //                 console.log('---------------------------- ')
 //             }
 //         })
+//     },
+//     rej => {
+//         console.log('REJ: something went wrong')
+//         console.log(rej)
 //     })
 
-/* getDiaDeSorte()
-    .then(res => {
-        resDiaDeSorte.push(...res)
-        console.log(resDiaDeSorte)
-    }) */
 
-// getTimeMania()
-//     .then(res => {
-//         resTimeMania.push(...res)
-//         console.log(resTimeMania)
-//     })
+const runAll = async () => {
+    await getSena()
+    await getQuina()
+    await getLotoFacil()
+    await getLotoMania()
+    await getSuperSete()
+    await getDuplaSena1()
+    await getDuplaSena2()
+    await getDiaDeSorte()
+    await getTimeMania()
+
+    const lotecaArray = []
+    let resLoteca = await getLoteca()
+    resLoteca = resLoteca[0].split('\n')
+    resLoteca.splice(0, 9)
+    while (resLoteca.length > 0) {
+        lotecaArray.push(
+            {
+                game: resLoteca.splice(0, 15)
+            }
+        )
+    }
+    lotecaArray.forEach(el => {
+        const golTimeA = String(el.game[1]).trim()
+        const timeA = String(el.game[4]).trim()
+        const timeB = String(el.game[9]).trim()
+        const golTimeB = String(el.game[11]).trim()
+        if (timeA !== "undefined" ){
+            console.log(timeA, golTimeA, ' x ', golTimeB, timeB)
+            console.log('---------------------------- ')
+        }
+    })
+}
+
+runAll()
 
